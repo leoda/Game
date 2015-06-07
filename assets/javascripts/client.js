3eaa1475ada4ece25eb12dc11990a6ed582b18d0
leoda <leoda94@mail.ru>
2015-06-07 10:10:34 +0000

    добавление css анимации

--- assets/javascripts/client.js
+++ assets/javascripts/client.js
@@ -1,12 +1,47 @@
-$(document).ready(function() {
-    var socket = io.connect('http://js-leoda.c9.io/');    
-    $('img').on("click",function(){
-      socket.emit('hello_message','Привет')      
+// $(document).ready(function() {
+//     var socket = io.connect('http://js-leoda.c9.io/');    
+//     $('img').on("click",function(){
+//       socket.emit('hello_message','Привет')      
+//     })
+//     socket.on('youPush',function(A){
+//         console.log(A);
+//     })
+// });
+
+$( document ).ready(function() {
+    var socket = io.connect('http://js-leoda.c9.io/');
+        
+    $('.post').on('click', function(){
+        $('.comment').val()
+        socket.emit('new message', {user:document.getElementById('name').value,message:$('.comment').val()})
+        $('.content').html()+$('.content').append('<b>'+document.getElementById('name').value+'</b>: <p>'+$('.comment').val()+'</p><br><br>')
+        $('.comment').val('')
     })
-    socket.on('youPush',function(A){
-        console.log(A);
+    socket.on('message for all', function(createNewMessage){
+        $('.content').html()+$('.content').append('<b>'+createNewMessage.user+'</b>: <p>'+createNewMessage.message+'</p><br><br>')
     })
+    $('.button').on('click', function(){
+        var myTextField = document.getElementById('name');
+	    socket.emit('New', myTextField.value)
+
+        $('form').animate({opacity:0}, 500);
+        $('.chat').animate({opacity:1, zIndex:10}, 500);
+        $('.menu').animate({opacity:1}, 500);
+ 
+        socket.on('disco', function(leftMan){
+            console.log(leftMan + " – игрок вышел из игры");
+        })
+    });
+     $('.play_pause').on('click', function(){
+        console.log("Щёлк!");
+       
+    })  
+    
+
 });
-
-
+        
+        
+        
+        
+       
 
--- server.js
+++ server.js
@@ -1,41 +1,92 @@
+// var express = require('express'),
+//     app = express(),
+//     server = require('http').Server(app),
+//     io = require('socket.io')(server),
+//     util = require('util'),
+//     jade = require('jade'),
+//     gamers = [];
+    
+
+// app.set('views', './views');
+// app.set('view engine', 'jade');
+// app.use(express.static("assets"));
+
+// app.get('/', function(req,res){
+//     res.render('index');
+// });
+
+// io.on("connection",function(socket){
+//     console.log('hello new user');
+//     // gamers.push(socket.id);
+    
+//     socket.on("disconnect",function(){
+//     console.log('bye my user!')
+//     //     console.log(gamers.indexOf(socket.id));
+//     //     console.log(gamers);
+//     //     gamers.splice(gamers.indexOf(socket.id),1)
+//     //     console.log(gamers);
+//     //     socket.broadcast.emit('playerLeft',socket.id);
+//     });
+//     socket.on('hello_message',function(data){
+//         socket.broadcast.emit("youPush", 'Кто то кликнул на мед, но не вы.');
+//         socket.emit('youPush', 'вы кликнули на мед, привет от сервера.');
+//         io.sockets.emit('youPush', 'Ура я всем разослал клик');
+//     })
+// });
+
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     util = require('util'),
     jade = require('jade'),
-    gamers = [];
     
+    gamers = [];
+
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.use(express.static("assets"));
-
-
 app.get('/', function(req,res){
     res.render('index');
 });
 
 
-
-io.on("connection",function(socket){
-    console.log('hello new user');
-    // gamers.push(socket.id);
+io.on("connection", function(socket){
+    console.log(socket.id + "connected")
     
-    socket.on("disconnect",function(){
-    console.log('bye my user!')
-    //     console.log(gamers.indexOf(socket.id));
-    //     console.log(gamers);
-    //     gamers.splice(gamers.indexOf(socket.id),1)
-    //     console.log(gamers);
-    //     socket.broadcast.emit('playerLeft',socket.id);
-    });
-    socket.on('hello_message',function(data){
-        socket.broadcast.emit("youPush", 'Кто то кликнул на мед, но не вы.');
-        socket.emit('youPush', 'вы кликнули на мед, привет от сервера.');
-        io.sockets.emit('youPush', 'Ура я всем разослал клик');
+    // socket.on('New', function(data) {
+    //     var infoNew = {};
+    //     infoNew.name = data;
+    //     infoNew.id = socket.id;
+    //     // gamers.push(infoNew);
+    // })
+    
+    socket.on('new message', function(createNewMessage) {
+        // console.log("получил сообщение");
+        socket.broadcast.emit('message for all', createNewMessage);
+        // console.log("отправил сообщение");
     })
-});
+   
+    socket.on('disconnect', function() {
+        console.log('bye my user!')
+        // console.log(gamers.indexOf(socket.id));
+        // console.log(gamers);
+        for(var i=0;i<gamers.length;i++){
+            if(gamers[i].id==socket.id){
+                socket.broadcast.emit('disco',gamers[i].name);
+                gamers.splice(gamers.indexOf(gamers[i]),1);
+                // delete gamers[i]
+                // console.log(gamers);
+            }
+        }
+    });
+    
+})
+
+
+
+
 
 server.listen(process.env.PORT, process.env.HOST);
 
